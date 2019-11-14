16133e98e7d91b6e012a620bbd6344660b63fee3
Alex DeKinder <ad3540@columbia.edu>
2019-11-13 19:22:00 -0500

    Create Train_Test_Split.R

---
+++ doc/Train_Test_Split.R
@@ -0,0 +1,15 @@
+#Split Train and Test Set
+library(splitstackshape)
+
+set.seed(1)
+
+test.users <- stratified(data,c("userId"),5)
+hold<-anti_join(data,test.users)
+test.movies <- stratified(hold,c("movieId"),5)
+
+data_test <- rbind(test.users,test.movies)
+data_train <- anti_join(data,data_test)
+
+
+write.csv(data_train,file = ".../output/data_train.csv")
+write.csv(data_test,file = ".../output/data_test.csv")
